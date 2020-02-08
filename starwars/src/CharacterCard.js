839c51c7998b89eeeb818967691fa1b6fe776d3a
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-02-07 23:45:08 -0500

    MVP

--- starwars/src/CharacterCard.js
+++ starwars/src/CharacterCard.js
@@ -1,15 +1,28 @@
 import React, { useState, useEffect} from 'react';
 import Reactstrap, { Card, CardText, CardTitle, CardHeader, CardBody } from 'reactstrap';
-
+import Axios from "axios";
+import Films from "./Films"
 const CharacterCard = props => {
   const [data, setData] = useState(props)
+  const [films, setFilms] = useState()
+
+  useEffect(() => {
+    const tempFilmsArray = [];
+    data.character.films.map(element => {
+      return Axios.get(element).then(response => {
+        tempFilmsArray.push(response.data)
+      })
+    })
+    setFilms(tempFilmsArray);
+  },[props.character])
 
   return <Card>
     <CardHeader>{data.character.name}</CardHeader>
     <CardBody>
       <CardText>Date of birth: {data.character.birth_year}</CardText>
       <CardText>Eye color: {data.character.eye_color}</CardText>
-      <CardText>{data.character.films}</CardText>
+      {/* <Films films={films}/> NOT WORKING BECAUSE REACT IS SHIT */}
+      <CardText>Height: {data.character.height}</CardText>
     </CardBody>
   </Card>
 }
--- starwars/src/CharactersContainer.js
+++ starwars/src/CharactersContainer.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import CharacterCard from "./CharacterCard";
+import Reactstrap, {Button} from "reactstrap"
 
 const CharactersContainer = () => {
   const [charactersData, setCharactersData] = useState([]);
+  const [nextPage, setNextPage] = useState();
+  const [previousPage, setPreviousPage] = useState();
 
   useEffect(() => {
     Axios.get("https://swapi.co/api/people")
       .then(response => {
         setCharactersData(response.data.results);
+        setNextPage(response.data.next)
+        setPreviousPage(response.data.previous)
         console.log(response);
       })
       .catch(error => {
@@ -17,6 +22,10 @@ const CharactersContainer = () => {
   }, []);
 
   return <div className="CharactersContainer">
+      <Button onclick={() => {
+
+      }}>Previous</Button>
+      <Button>Next</Button>
       {charactersData.map(element => {
           return <CharacterCard character={element} key={Math.random()}/>
       })}
